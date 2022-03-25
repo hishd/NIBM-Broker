bd927acd6fe142ecb48cfc48f06173db4d87e848
HishD <hisharadilshan3@gmail.com>
2022-03-26 01:20:57 +0530

    Initial Commit with adding resources

--- NIBM Broker/ContentView.swift
+++ NIBM Broker/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Hello, world!")
+                .font(Font.custom("gilroy-semibold", size: 20))
             .padding()
+            Text("Hello, world!")
+                .font(Font.custom("gilroy-bold", size: 20))
+            .padding()
+        }
     }
 }
 
