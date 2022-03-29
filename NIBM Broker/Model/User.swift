2243b02d5d0f5b2a94316ef6348d6ac97c3ed7c0
HishD <hisharadilshan3@gmail.com>
2022-03-29 07:27:47 +0530

    Added async user sign-in

--- NIBM Broker/Model/User.swift
+++ NIBM Broker/Model/User.swift
@@ -8,8 +8,13 @@
 import Foundation
 import FirebaseAuth
 
-struct User {
-    var userName: String
+enum SignInError: Error {
+    case operationError
+    case noUserFound
+}
+
+class User {
+    var nicNo: String
     var password: String
     var name: String
     var mobileNo: String
@@ -21,19 +26,40 @@ struct User {
     var locationLat: Double
     var locationLon: Double
     
+    init(nicNo: String, password: String, name: String, mobileNo: String, emailAddress: String, dobDay: Int, dobMonth: Int, dobYear: Int, gender: String, locationLat: Double, locationLon: Double) {
+        self.nicNo = nicNo
+        self.password = password
+        self.name = name
+        self.mobileNo = mobileNo
+        self.emailAddress = emailAddress
+        self.dobDay = dobDay
+        self.dobMonth = dobMonth
+        self.dobYear = dobYear
+        self.gender = gender
+        self.locationLat = locationLat
+        self.locationLon = locationLon
+    }
+    
+    convenience init(emailAddress: String, password: String) {
+        self.init(nicNo: "", password: password, name: "", mobileNo: "", emailAddress: emailAddress, dobDay: 0, dobMonth: 0, dobYear: 0, gender: "", locationLat: 0, locationLon: 0)
+    }
+    
     var isSignedIn: Bool {
         let auth = Auth.auth()
         return auth.currentUser != nil
     }
     
-    func signIn(emailAddress: String, password: String, completion: @escaping (Bool) -> (Void)) {
-        let auth = Auth.auth()
-        auth.signIn(withEmail: emailAddress, password: password) { result, error in
-            guard result != nil, error == nil else {
-                return
+    func signInAsync(emailAddress: String, password: String) async throws -> Bool {
+        try await withCheckedThrowingContinuation { continuation in
+            let auth = Auth.auth()
+            auth.signIn(withEmail: emailAddress, password: password) { [weak self] result, error in
+                guard result != nil, error == nil else {
+                    continuation.resume(throwing: SignInError.operationError)
+                    return
+                }
+                
+                continuation.resume(returning: true)
             }
-            
-            completion(true)
         }
     }
 }
